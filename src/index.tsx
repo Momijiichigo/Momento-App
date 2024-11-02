e3f74b971240f3cdf291c54479ea2e3a6e39d8c6
Momijiichigo <ichigomomiji436@gmail.com>
2024-11-02 13:34:30 -0500

    pages

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,13 @@ import { render } from "solid-js/web";
 import "./index.css";
 import {Route, Router} from "@solidjs/router";
 import {Home} from "./pages/Home";
+import {DiscoveredPage} from "./pages/Discovered";
 // import { ChatList } from "./pages/ChatList";
 // import { ChatScreen } from "./pages/ChatScreen";
 
 render(
   () => <Router>
-    <Route path="/" component={Home}/>
+    <Route path="/" component={DiscoveredPage}/>
 
   </Router>,
   document.getElementById("root") as HTMLElement
---
+++ src/pages/Discovered.tsx
@@ -0,0 +1,10 @@
+
+import {Component} from "solid-js";
+
+export const DiscoveredPage: Component = () => {
+  return (
+    <div>
+      <h1>Your discovered momentos</h1>
+    </div>
+  );
+}
