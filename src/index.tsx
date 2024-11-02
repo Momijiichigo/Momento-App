7c85ba63b22f2c8b52018db1422c7d7b8dcdbc22
김승중 <kim@wireless-10-150-27-137.public.utexas.edu>
2024-11-02 13:44:58 -0500

    add here map npm registry

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,14 @@ import "./index.css";
 import {Route, Router} from "@solidjs/router";
 import {Home} from "./pages/Home";
 import {DiscoveredPage} from "./pages/Discovered";
+import { MapTest } from "./pages/MapTest";
 // import { ChatList } from "./pages/ChatList";
 // import { ChatScreen } from "./pages/ChatScreen";
 
 render(
   () => <Router>
     <Route path="/" component={DiscoveredPage}/>
-    <Route path="/maptest" component={DiscoveredPage}/>
+    <Route path="/maptest" component={MapTest}/>
     
 
   </Router>,
