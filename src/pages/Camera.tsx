3f0a4110760bdfb811407b26e375c6fca334bf67
김승중 <kim@wireless-10-150-27-137.public.utexas.edu>
2024-11-03 00:50:39 -0500

    going home link

--- src/pages/Camera.tsx
+++ src/pages/Camera.tsx
@@ -1,7 +1,9 @@
+import { A } from "@solidjs/router";
 import { Component } from "solid-js";
 
 export const CameraPage: Component = () => {
     return <div>
+        <A href="/">Home</A>
         <label for="selfie">Take a picture of your face:</label>
         <input type="file" id="selfie" name="selfie" accept="image/*" capture="user" />
         <label for="picture">Take a picture using back facing camera:</label>
