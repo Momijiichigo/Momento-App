d17832293d187c5d13e6f74657471c9fa5444861
Kim-Seungjung <sjk1q2w3e4r@naver.com>
2024-11-04 23:31:12 -0600

    fix jsx error

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -106,7 +106,6 @@ export const Login: Component = () => {
                   }
                 }}
                 type="button"
-
                 class="w-full flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
                 Sign up
@@ -117,5 +116,4 @@ export const Login: Component = () => {
       </div>
     </div>
   )
-
 }
